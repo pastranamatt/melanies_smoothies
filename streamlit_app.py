7b702a690928c68073e31a9f1d60e97580ef9dea
pastranamatt <72945201+pastranamatt@users.noreply.github.com>
2025-02-14 14:02:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 #st.stop()
 
 # Convert
-pd_df=my_dataframe.topandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
